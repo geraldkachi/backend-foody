f23a42efc09f20de91ad5874c2dc3beb47494f77
geraldkachi <fitzgeraldkachi@gmail.com>
2024-07-02 09:27:25 +0100

    fixes

--- server.ts
+++ server.ts
@@ -1,9 +1,38 @@
 import express from "express";
+import fs from "fs";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 const CategoryRoute = require('./routes/category')
 const app = express();
 const port = process.env.PORT || 3000;
+
+const hbs = require('hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+
+
+// middleware 
+app.use((req, res, next) => {
+  var now = new Date(); 
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log)
+  fs.appendFile('server.log', log + '/n', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+    next();
+  })}
+);
+
+// app.use((req, res, next) => {
+//   res.render('maintiance.hbs')
+// })
+hbs.registerHelper('streamIt', (text: string) => {
+  return text.toUpperCase() 
+})
+hbs.registerHelper('getFullYear', () => {
+  return new Date().getFullYear() 
+})
+app.set('view engine', 'hbs')
+
 
 dotenv.config()
 // Current IP Address (102.89.47.65/32) added!
@@ -19,7 +48,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('categories', CategoryRoute)
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    // header: 'Header',
+    welcomeMessage: 'Welcome to Home Page',
+    currentYear: new Date().getFullYear(),
+  });
 });
+
+8757E111
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: "unable to fulfill request"
+  });
+});
+app.get('/about', (req, res) => {
+  res.send({
+    name: 'Foodly',
+    version: '1.0.0',
+    description: 'Foodly is an app that helps you find the best restaurants in your area.',
+    author: 'Foodly Team',
+    contact: 'https://github.com/Foodly-Team',
+    license: 'MIT',
+  });
+});
+
+app.use(express.static(__dirname + '/public'))
 app.listen(process.env.PORT || 6013, () => console.log(`Backend Foodie Server App running on port: ${process.env.PORT || 6013}!` ));
+
+
+// yarn --frozen-lockfile install
